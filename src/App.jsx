24f2d9d7d418d8803bb9174d272161ceb6c340a0
fateme-shabani <shabanifateme22@gmail.com>
2023-12-18 17:08:58 +0330

    edit form items

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import Form from "./components/Form";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const products = [
+  const [products, setproducts] = useState([
     { id: 1, name: " لپ تاپ hp", price: 54000 },
     { id: 2, name: "  تلفن همراه ", price: 14600 },
     { id: 3, name: "  تبلت سامسونگ", price: 23000 },
     { id: 4, name: " ایرپاد شیائومی", price: 8000 },
-  ];
+  ]);
+  // const products = [
+  //   { id: 1, name: " لپ تاپ hp", price: 54000 },
+  //   { id: 2, name: "  تلفن همراه ", price: 14600 },
+  //   { id: 3, name: "  تبلت سامسونگ", price: 23000 },
+  //   { id: 4, name: " ایرپاد شیائومی", price: 8000 },
+  // ];
+  const addItemToList = (item) => {
+    setproducts(...products, item);
+  };
 
   return (
     <>
       <div className="product-style">
-      <Form />
+        <Form addItemToList={addItemToList} />
         {products.map((item) => {
           return <Product key={item.id} products={item} />;
         })}
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 
-const Form = () => {
+const Form = (props) => {
   const [formData, setformData] = useState({ name: "", price: 0 });
 
   const formHandler = (e) => {
     e.preventDefault()
     console.log(formData)
+    props.addItemToList(formData)
     };
 
   const nameHandler = (e) => {
