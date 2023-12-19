e09e743942b95175b3dab0f6780d7620a0ac62bd
fateme-shabani <shabanifateme22@gmail.com>
2023-12-19 14:21:44 +0330

    fix add item

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useId, useState } from "react";
 import Product from "./components/Product";
 import Form from "./components/Form";
 import "./App.css";
+import {v4 as uuidv4} from 'uuid'
+
 
 function App() {
+  // const id=useId()
+  // console.log(id)
   const [products, setproducts] = useState([
     { id: 1, name: " لپ تاپ hp", price: 54000 },
     { id: 2, name: "  تلفن همراه ", price: 14600 },
@@ -18,6 +22,10 @@ function App() {
   // ];
   const addItemToList = (item) => {
     setproducts([...products, item]);
+    item.id=uuidv4()
+    // item.id=new Date()
+    // console.log(item)
+    
   };
 
   return (
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,37 @@
 import React, { useState } from "react";
 
-const Form = ({addItemToList}) => {
+const Form = ({ addItemToList }) => {
   const [formData, setformData] = useState({ name: "", price: 0 });
 
   const formHandler = (e) => {
-    e.preventDefault()
-    console.log(formData)
-    addItemToList(formData)
-    };
+    e.preventDefault();
+    console.log(formData);
+    addItemToList(formData);
+    setformData({ name: "", price: 0 });
+  };
 
   const nameHandler = (e) => {
-    setformData({...formData , name:e.target.value})
+    setformData({ ...formData, name: e.target.value });
   };
   const priceHandler = (e) => {
-    setformData({...formData , price:e.target.value})
+    setformData({ ...formData, price: e.target.value });
   };
   return (
     <>
       <div>
         <form action="#" onSubmit={formHandler}>
-          <input type="text" onChange={nameHandler} className="input-style"></input>
-          <input type="number" onChange={priceHandler} className="input-style"></input>
+          <input
+            type="text"
+            onBlur={nameHandler}
+            value={formData.name}
+            className="input-style"
+          ></input>
+          <input
+            type="number"
+            onBlur={priceHandler}
+            value={formData.price}
+            className="input-style"
+          ></input>
           <button>add</button>
         </form>
       </div>
